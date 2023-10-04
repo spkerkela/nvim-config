3d3264c2638203b353efed9230d6220209db1842
Simo-Pekka Kerkelä <simopekka.kerkela@spkerkela.com>
2023-10-05 00:23:00 +0300

    add telescope

--- lua/spkerkela/packer.lua
+++ lua/spkerkela/packer.lua
@@ -2,4 +2,8 @@ vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
     use 'wbthomason/packer.nvim'
+    use {
+        'nvim-telescope/telescope.nvim', tag = '0.1.x',
+        requires = {{'nvim-lua/plenary.nvim'}}
+    }
 end)
