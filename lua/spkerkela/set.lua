51432bf4aa862b2129480073f16f0b4b76ff669a
Simo-Pekka Kerkelä <simopekka.kerkela@spkerkela.com>
2023-10-04 23:40:14 +0300

    add some more config options

--- lua/spkerkela/set.lua
+++ lua/spkerkela/set.lua
@@ -9,3 +9,12 @@ vim.opt.smartindent = true
 
 vim.opt.swapfile = false
 vim.opt.backup = false
+
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
+vim.opt.updatetime = 50
+
+vim.opt.colorcolumn = "80"
+
+vim.opt.scrolloff = 24
