e592a16627a32ba735c3507e33a399cfc6916077
Simo-Pekka Kerkelä <simo-pekka.kerkela@reaktor.com>
2023-10-05 13:39:11 +0300

    fix copy-paste config

--- lua/spkerkela/remap.lua
+++ lua/spkerkela/remap.lua
@@ -1,3 +1,3 @@
-vim.keymap.set("", "<leader>y", "\"+y")
-vim.keymap.set("", "<leader>y", "\"+y")
-vim.keymap.set("", "<leader>Y", "\"+Y")
+vim.keymap.set("n", "<leader>y", "\"+y")
+vim.keymap.set("v", "<leader>y", "\"+y")
+vim.keymap.set("n", "<leader>Y", "\"+Y")
